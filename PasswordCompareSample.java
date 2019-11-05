2db506fae096ec06120bb21d532e60dca5ebd51c
urbennoroac <42480295+urbennoroac@users.noreply.github.com>
2019-11-04 23:28:19 -0600

    Add files via upload
    Actividad 4 y 5 Practica 11

---
+++ PasswordCompareSample.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.Arrays;
+public class PasswordCompareSample{
+	public static void main (String [] args) throws NumberFormatException,IOException{
+		Console console = System.console();
+		if(console == null){
+			throw new RuntimeException("Console not available");
+		}else{
+			char [] password=console.readPassword ("Enter your password: ");
+			console.format("Enter your password again: ");
+			console.flush();
+			char [] verify=console.readPassword();
+			boolean match = Arrays.equals(password,verify);
+	//Immediately clear passwords from memory
+			for(int i =0;i<password.length;i++){
+				password[i]='x';
+			}
+			for(int i =0;i<verify.length;i++){
+				verify[i]='x';
+			}
+			console.format("Your password was "+(match ? "correct\n" : "incorrect\n"));
+		}
+	}
+}
